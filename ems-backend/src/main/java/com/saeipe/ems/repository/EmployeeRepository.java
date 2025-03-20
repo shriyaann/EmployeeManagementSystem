de0cf3053e66f523aeae169c1d8df0c184775332
Shriya Ann <shriya.ann@gmail.com>
2025-03-19 21:54:48 -0400

    ems-backend commit undo
    Signed-off-by: Shriya Ann <shriya.ann@gmail.com>

--- ems-backend/src/main/java/com/saeipe/ems/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.saeipe.ems.repository;
-
-import com.saeipe.ems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
